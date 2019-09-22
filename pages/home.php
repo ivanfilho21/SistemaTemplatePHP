d48aba77ef28dfa008deb95f5ced54958d705885
Ivan Filho <ivanfilho21@gmail.com>
2019-09-22 10:59:27 -0300

    Create pages

---
+++ pages/home.php
@@ -0,0 +1,10 @@
+<div class="container text-center">
+    <div class="row justify-content-center">
+        <div class="col-md-7">
+            <h1 class="font-weight-light mt-4 text-white">Welcome</h1>
+            <p class="lead text-white-50">
+                This is the Home Page.
+            </p>
+        </div>
+    </div>
+</div>
---
+++ pages/layout/app.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="<?= $lang ?>">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="pages/layout/app.css">
+</head>
+<body class="d-flex flex-column">
+    <header class="header">
+        <nav class="navbar navbar-expand navbar-light bg-light">
+            <a class="navbar-brand" href="./home">
+                <span style="color: dodgerblue;">OO</span>
+                <span style="font-weight: bold; color: firebrick;">Test</span>
+            </a>
+        </nav>
+    </header>
+
+    <main class="main py-3">
+        <!-- col-12 col-md-9 col-xl-8 py-3 pl-md-5 -->
+        <?php
+        
+        $view = $page;
+        $view .= (strpos(".php", $view) !== false) ? ".php" : "";
+        // echo $view;
+        require "pages/$page.php";
+        ?>
+        
+    </main>
+
+    <footer class="footer py-4 bg-dark text-white-50">
+        <div class="container text-center">
+            <small>Copyright &copy; 2019 - OO Test</small>
+        </div>
+    </footer>
+</body>
+</html>
